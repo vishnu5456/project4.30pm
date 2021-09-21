f7a0ce88ab0881cd6fa60781dbfabb2a84cab74f
vishnu5456 <jithvishnu46@gmail.com>
2021-09-21 17:53:28 +0530

    some statement added

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,18 @@
+package org.company;
+
+public class CompanyInfo {
+private void companyName(long salary,String name) {
+System.out.println("company name : "+name);
+System.out.println("company salary: "+salary);
+}
+private void companyName(String address,long plotno) {
+System.out.println("company address : "+address);
+System.out.println("company plotno: "+plotno);
+}
+public static void main(String[] args) {
+	CompanyInfo c= new CompanyInfo();
+	c.companyName(50000l, "zoho");
+	c.companyName("chennai", 10l);
+	
+}
+}
